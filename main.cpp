e8156952e4bec7b5ee6c7e98a830ea1e6cb95dc2
Nermeen289 <63092832+Nermeen289@users.noreply.github.com>
2020-04-14 20:41:37 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,93 +1,43 @@
 #include <iostream>
-
+#include <set>
 using namespace std;
-template <class t>
-class Stack
+set<string>str;
+set<string> ::iterator it;
+
+void recprmute(string sofar,string rest)
+
 {
- public:
-    struct node
+
+    if(rest=="")
+     str.insert(sofar);
+    else
     {
-        t item;
-        node *next;
-    };
-    node *top;
-   Stack()
-    {
-        top=nullptr;
-    }
-    void push(t element)
-    {
-        node *newitem=new node;
-        if(newitem==nullptr)
+         for (int i = 0; i < rest.length(); i++)
         {
-            cout<<"can't allocate memory"<<endl;
+           string next = sofar + rest[i];
+           string remaining = rest.substr(0, i)+ rest.substr(i+1);
+              recprmute(next, remaining);
         }
-        else
-        {
-            newitem->item=element;
-            newitem->next=top;
-            top=newitem;
-        }
-
     }
-   bool isempty()
-   {
-       return top==nullptr;
-   }
-    void pop(t&element)
-    {
-        if(isempty())
-            cout<<"stack is empty"<<endl;
-        else
-            {
-                element=top->item;
-                node *temp=top;
-                top=top->next;
-                temp=temp->next=nullptr;
-                delete temp;
-            }
 
 
-    }
-     void getelement(t&element)
-    {
-        if(isempty())
-            cout<<"stack is empty"<<endl;
-        else
-            {
-                element=top->item;
+}
+    void ListPermutations(string s){
 
-            }
+        recprmute("",s);
+        cout<<"Output"<<endl;
+        for(it=str.begin();it!=str.end();it++)
+            cout<<*it<<" ";
+}
 
-
-    }
-    void display()
-    {
-        node *head=new node;
-        head=top;
-        cout<<"[";
-        while (head!=nullptr)
-        {
-            cout<<head->item <<" ";
-            head=head->next;
-        }
-        cout<<"]"<<endl;
-
-    }
-};
 int main()
 {
-    stack<int>s;
-       s.push(50);
-        s.push(60);
-         s.push(70);
-          s.push(80);
-     s.display();
-     int x=0;
-     s.pop(x);
-     s.display();
-     s.getelement(x);
-     cout<<x<<endl;
 
-        return 0;
+    string s;
+    cout<<"Input:"<<endl;
+    cin>>s;
+    ListPermutations(s);
+
+
+ return 0;
 }
